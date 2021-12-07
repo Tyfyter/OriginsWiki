208c8e8748a5446bec6732bd7dd6587fe7256e2f
Tyfyter <tyfyter2616@gmail.com>
2021-12-07 17:25:15 -0500

    Update automarkdown.js

--- automarkdown.js
+++ automarkdown.js
@@ -1,22 +1,42 @@
 //window.alert("test");
-
-function getResponse() {
-	// `this` will refer to the `XMLHTTPRequest` object that executes this function
-	var responseObj = JSON.parse(this.responseText);
-	var text = "";
-	for(var i = 0; i < responseObj.tree.length; i++){
-		var addition = responseObj.tree[i].path.replace(/\.[^.]+/g, "");
-		if(addition){
-			text+=addition+"<br>";
+function createFilteredResponseHandler(filter, action, includeExtension){
+	function getResponse() {
+		// `this` will refer to the `XMLHTTPRequest` object that executes this function
+		var responseObj = JSON.parse(this.responseText);
+		var values = [];
+		for(var i = 0; i < responseObj.tree.length; i++){
+			var match = responseObj.tree[i].path.match(/\.[^.]+/g);
+			var addition = responseObj.tree[i].path.replace(match, "");
+			if(addition && (!filter || match==filter)){
+				if(includeExtension){
+					values.push(addition+match);
+				}else{
+					values.push(addition);
+				}
+			}
 		}
+		action(values);
 	}
-	document.getElementById("content").innerHTML += "<div>"+text+"</div>";
+	return getResponse;
 }
 
-var request = new XMLHttpRequest();
-request.onload = getResponse;
-request.open('get', 'https://api.github.com/repos/Tyfyter/OriginsWiki/git/trees/main', true);
-//request.open('get', 'https://api.github.com/repos/Tyfyter/OriginsWiki/commits/main', true);
-//request.open('get', 'https://api.github.com/users/Tyfyter/repos/OriginsWiki', true);
-request.send();
+function appendAllToContent(values){
+	values = values.map(function(v){
+		return "<a href="+v+">"+v+"</a>";
+	});
+	document.getElementById("content").innerHTML += "<div>"+values.join("<br>")+"</div>";
+}
 
+function requestAndProcessPageList(action, filter){
+	if(!filter){
+		filter = ".html";
+	}
+	var request = new XMLHttpRequest();
+	request.onload = createFilteredResponseHandler(filter, action, true);
+	request.open('get', 'https://api.github.com/repos/Tyfyter/OriginsWiki/git/trees/main', true);
+	//request.open('get', 'https://api.github.com/repos/Tyfyter/OriginsWiki/commits/main', true);
+	//request.open('get', 'https://api.github.com/users/Tyfyter/repos/OriginsWiki', true);
+	request.send();
+}
+
+requestAndProcessPageList(appendAllToContent);
