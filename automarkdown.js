34a0c19eaab268d04d64eb657c338866d802e231
Tyfyter <tyfyter2616@gmail.com>
2021-12-07 18:30:48 -0500

    search functions at least partially working

--- automarkdown.js
+++ automarkdown.js
@@ -1,4 +1,5 @@
 //window.alert("test");
+
 function createFilteredResponseHandler(filter, action, includeExtension){
 	function getResponse() {
 		// `this` will refer to the `XMLHTTPRequest` object that executes this function
@@ -27,16 +28,40 @@ function appendAllToContent(values){
 	document.getElementById("content").innerHTML += "<div>"+values.join("<br>")+"</div>";
 }
 
-function requestAndProcessPageList(action, filter){
+function requestAndProcessPageList(action, filter, sync){
 	if(!filter){
 		filter = ".html";
 	}
 	var request = new XMLHttpRequest();
 	request.onload = createFilteredResponseHandler(filter, action, true);
-	request.open('get', 'https://api.github.com/repos/Tyfyter/OriginsWiki/git/trees/main', true);
+	request.open('get', 'https://api.github.com/repos/Tyfyter/OriginsWiki/git/trees/main', !sync);
 	//request.open('get', 'https://api.github.com/repos/Tyfyter/OriginsWiki/commits/main', true);
 	//request.open('get', 'https://api.github.com/users/Tyfyter/repos/OriginsWiki', true);
 	request.send();
 }
 
-requestAndProcessPageList(appendAllToContent);
+function getSearchLinks(query, filter){
+	if(!filter){
+		filter = ".html";
+	}
+    query = query.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+	var regexQuery = new RegExp("("+query+")","gi");
+	var results = [];
+	requestAndProcessPageList(function(re){
+		for(var i = 0; i < re.length; i++){
+			if(re[i].match(regexQuery)){
+				results.push(re[i]);
+			}
+		}
+	}, filter, true);
+	return "<div>"+results.map(function(v){
+		return "<a href="+v+">"+v.replace(/\.[^.]+/g, "").replace(regexQuery, "<b>\$1</b>")+"</a>";
+	}).join("<br>")+"</div>"
+}
+
+//requestAndProcessPageList(appendAllToContent);
+
+// do things after the DOM loads fully
+window.addEventListener("load", function () {
+	document.getElementById("content").innerHTML += getSearchLinks("pa");
+});
