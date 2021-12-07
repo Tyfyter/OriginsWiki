bc2fb05502fe8ee61d8f9800b4b5ba20780c416e
Tyfyter <tyfyter2616@gmail.com>
2021-12-07 16:17:06 -0500

    demonstration of file listing

--- automarkdown.js
+++ automarkdown.js
@@ -1,1 +1,22 @@
-window.alert("test");
+//window.alert("test");
+
+function getResponse() {
+	// `this` will refer to the `XMLHTTPRequest` object that executes this function
+	var responseObj = JSON.parse(this.responseText);
+	var text = "";
+	for(var i = 0; i < responseObj.tree.length; i++){
+		var addition = responseObj.tree[i].path.replace(/\.[^.]+/g, "");
+		if(addition){
+			text+=addition+"<br>";
+		}
+	}
+	document.getElementById("content").innerHTML += "<div>"+text+"</div>";
+}
+
+var request = new XMLHttpRequest();
+request.onload = getResponse;
+request.open('get', 'https://api.github.com/repos/Tyfyter/OriginsWiki/git/trees/main', true);
+//request.open('get', 'https://api.github.com/repos/Tyfyter/OriginsWiki/commits/main', true);
+//request.open('get', 'https://api.github.com/users/Tyfyter/repos/OriginsWiki', true);
+request.send();
+
